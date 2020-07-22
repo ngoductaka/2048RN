ee1b71c1bbce2698aa269b7925c6bd712e050fb1
ngoc duc <ngocduc@ngocs-MacBook-Pro.local>
2020-07-22 23:02:45 +0700

    gg sign in

--- src/app.js
+++ src/app.js
@@ -8,9 +8,47 @@ import {
 } from 'react-native';
 import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 import { get, cloneDeep } from 'lodash';
-import { LoginButton, AccessToken, LoginManager } from 'react-native-fbsdk';
+import { LoginButton, AccessToken, LoginManager } from 'react-native-fbsdk'; import {
+	GoogleSignin,
+	GoogleSigninButton,
+	statusCodes,
+} from '@react-native-community/google-signin';
+
+GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+// GoogleSignin.configure({
+// 	forceConsentPrompt: true
+// });
+  GoogleSignin.configure({
+	forceConsentPrompt: true,
+	// scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+	webClientId: '275839170462-1i2frn4dtshpvf0cl6ott8di8ciosjeo.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+	// offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+	// hostedDomain: '', // specifies a hosted domain restriction
+	// loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+	// forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+	// accountName: '', // [Android] specifies an account name on the device that should be used
+	iosClientId: '275839170462-12ite4mr9ku6ai0385j5f56etnpcl1fc.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  });
 
 import AwesomeAlert from 'react-native-awesome-alerts';
+signIn = async () => {
+	try {
+		await GoogleSignin.hasPlayServices();
+		const userInfo = await GoogleSignin.signIn();
+		// this.setState({ userInfo });
+		console.log('userInfouserInfo', userInfo)
+	} catch (error) {
+		if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+			// user cancelled the login flow
+		} else if (error.code === statusCodes.IN_PROGRESS) {
+			// operation (e.g. sign in) is in progress already
+		} else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+			// play services not available or outdated
+		} else {
+			// some other error happened
+		}
+	}
+};
 
 const handleCheckLoose = (data) => {
 	for (let row = 3; row >= 0; row--) {
@@ -148,9 +186,12 @@ const dnd = () => {
 			}}
 		>
 			<View style={styles.container}>
-				<TouchableOpacity onPress={handleLoginFake} > 
+				<TouchableOpacity onPress={handleLoginFake} >
 					<Text> login facebook </Text>
 				</TouchableOpacity>
+				<GoogleSigninButton onPress={() => {
+					signIn()
+				}} />
 				<Text style={{ fontSize: 16 }}> Score: {reload}</Text>
 				<View style={styles.main}>
 					{
